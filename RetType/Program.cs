d2d370f6b28a716d402297b88f4d7a06365c2e3e
Adrian Sims <adriansims62781@gmail.com>
2021-02-08 12:19:20 +0100

    Add project files.

---
+++ RetType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace RetType
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var n = GetL<int>();
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+
+        private static object GetL<T>()
+        {
+            var ret = Get();
+            return ret is T t ? t : throw new Exception($"Wrong type - expected {typeof(T)} but received {ret.GetType()}");
+        }
+
+        private static object Get()
+        {
+            return 1L;
+        }
+    }
+}
